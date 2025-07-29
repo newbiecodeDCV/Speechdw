77c56f71a82c9f38171fbcc8be2056e5479c0def
newbiecodeDCV <nguyentrungtrieu080705@gmail.com>
2025-07-29 14:51:39 +0700

    fix

--- utils/audio_dataset.py
+++ utils/audio_dataset.py
@@ -10,20 +10,22 @@ class AudioDataset(Dataset):
         self.data = pd.read_csv(csv_path)
         self.audio_dir = audio_dir
         self.transform = transform
-        self.label_map =  {label: idx for idx, label in enumerate(self.data['gender'].unique())}
+        self.label_map =  {label: idx for idx, label in enumerate(self.data['sex'].unique())}
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        file_path = self.data.iloc[idx]['file_path']
-        label = self.label_map[self.data.iloc[idx]['gender']]
+        file_path = self.data.iloc[idx]['audio_path']
+        label = self.label_map[self.data.iloc[idx]['sex']]
         file_path = os.path.join(self.audio_dir, file_path)
         waveform, sample_rate = torchaudio.load(file_path)
         if self.transform:
             waveform = self.transform(waveform)
 
         return waveform.squeeze(0).T, label  # (Time, Feature)
+    def __getinfo__(self):
+        return self.label_map
 
 def get_collate_fn():
     def collate_fn(batch):
