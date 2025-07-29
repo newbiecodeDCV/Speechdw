ed2045b6b73fa9f1b55e2a2b8fc5bc601a834361
newbiecodeDCV <nguyentrungtrieu080705@gmail.com>
2025-07-29 14:43:24 +0700

    update

--- train.py
+++ train.py
@@ -26,8 +26,8 @@ mel_spec = torchaudio.transforms.MelSpectrogram(
     hop_length=512,
     n_mels=64
 )
-dataset = AudioDataset(csv_path=r'data\speaker_audio_samples.csv', audio_dir='data', transform=mel_spec)
-vaild_set = AudioDataset(csv_path=r'data\speaker_dev_audio_samples.csv',audio_dir='data', transform=mel_spec)
+dataset = AudioDataset(csv_path=r'/data/train_dataset.csv', audio_dir='/data', transform=mel_spec)
+vaild_set = AudioDataset(csv_path=r'/data/dev_dataset.csv',audio_dir='/data', transform=mel_spec)
 train_loader = DataLoader(dataset, batch_size=32, shuffle=True,collate_fn=get_collate_fn(),num_workers=4)
 valid_loader = DataLoader(vaild_set, batch_size=32, shuffle=True,collate_fn=get_collate_fn(),num_workers=4)
 
