57875def3534d4a3506c5d5ad4d90e8e3534eab2
newbiecodeDCV <nguyentrungtrieu080705@gmail.com>
2025-07-29 17:06:29 +0700

    update

--- utils/audio_dataset.py
+++ utils/audio_dataset.py
@@ -7,20 +7,12 @@ import torchaudio.transforms as T
 import os
 
 class AudioDataset(Dataset):
-    def __init__(self, csv_path, audio_dir, transform=None,augment=False):
+    def __init__(self, csv_path, audio_dir, transform=None):
         self.data = pd.read_csv(csv_path)
         self.audio_dir = audio_dir
         self.transform = transform
         self.label_map =  {label: idx for idx, label in enumerate(self.data['sex'].unique())}
-        self.augment = augment
-        if self.augment:
-                self.augment_transform = torch.nn.Sequential(
-                    T.Vol(gain=(0.5, 1.5), gain_type="amplitude"),
-                    T.FrequencyMasking(freq_mask_param=15),
-                    T.TimeMasking(time_mask_param=35),
-                )
-        else:
-                self.augment_transform = None
+
     def __len__(self):
         return len(self.data)
 
@@ -29,9 +21,6 @@ class AudioDataset(Dataset):
         label = self.label_map[self.data.iloc[idx]['sex']]
         file_path = os.path.join(self.audio_dir, file_path)
         waveform, sample_rate = torchaudio.load(file_path)
-        if self.augment_transform:
-            waveform = self.augment_transform(waveform)
-
         if self.transform:
             waveform = self.transform(waveform)
 
